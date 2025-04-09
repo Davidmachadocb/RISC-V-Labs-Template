a398b55fdfe860644f6df72c6125a72bde89aca0
Davidmachadocb <davidmachadocbec@gmail.com>
2025-04-09 13:34:23 -0300

    Lab 06 update

--- labs/lab06/main.c
+++ labs/lab06/main.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 
+#define POP 0
+#define PUSH 1
+
 extern void func(int a, int op, int* result);
 
 int main() {
     int arg = 42;
-    int op = 0;
     int result = 0;
 
-    func(arg, op, &result);
+    func(arg, PUSH, &result);
     printf("Pushed: %d\n", arg);
 
     arg = 12;
-    func(arg, op, &result);
+    func(arg, PUSH, &result);
     printf("Pushed: %d\n", arg);
 
-    op = 1;
-    func(arg, op, &result);
+    func(arg, POP, &result);
     printf("Popped: %d\n", result);
-    func(arg, op, &result);
+    
+    func(arg, POP, &result);
     printf("Popped: %d\n", result);
+
     return 0;
 }
